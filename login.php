a98cbe1038dfa9c4d64bea155f11491aa86c3b29
kwSYS <126925725+kwSYS@users.noreply.github.com>
2023-03-06 18:36:00 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,39 +1,45 @@
 <?php
+session_start(); // Start session
 
-// Start the session
+// Check if form is submitted
+if (isset($_POST['login'])) {
 
-include("db.php");
+    // Include database connection
+    require_once 'db.php';
 
-// Check if the user has submitted the form to create an account
+    // Sanitize user input
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
 
+    // Check if fields are empty
+    if (empty($username) || empty($password)) {
+        $_SESSION['error'] = "Username and password are required.";
+        header("Location: Login.htm");
+        exit();
+    }
 
-// Check if the form has been submitted
-if (isset($_POST['submit'])) {
-  // Get the form data
-  $username = mysqli_real_escape_string($conn, $_POST['Uname']);
-  $password = mysqli_real_escape_string($conn, $_POST['Password']);
-
-  // Check if the username and password are not empty
-  if (empty($username) || empty($password)) {
-    echo "Error: The broker and password fields cannot be empty.";
-  } else {
     // Hash the password
     $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-    // Check if the username already exists in the database
-    $query = "SELECT * FROM user WHERE User_Name = '$username'";
-    $result = mysqli_query($conn, $query);
-    if (mysqli_num_rows($result) > 0) {
-      // The username already exists, so display an error message
-      echo "Error: Broker exists already.";
+    // Check if username and password match
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$hashed_password'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        // Login successful
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['user_id'] = $row['id'];
+        $_SESSION['username'] = $row['username'];
+        header("Location: ClientCreation.html");
+        exit();
     } else {
-      // The username is available, so create the account
-      $query = "INSERT INTO user (User_Name, Pass_Word) VALUES ('$username', '$password_hash')";
-      mysqli_query($conn, $query);
-	  header("Location: Login.htm");
-      echo "Your account has been created successfully.";
-	  
+        // Login failed
+        $_SESSION['error'] = "Invalid username or password.";
+        header("Location: Login.htm");
+        exit();
     }
-  }
+} else {
+    header("Location: Login.htm");
+    exit();
 }
 ?>
