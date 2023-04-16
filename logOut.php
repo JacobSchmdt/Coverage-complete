0d81b8413c2964ba8cef962a9c02e5c1c0dec910
RenzCatt <90468595+RenzCatt@users.noreply.github.com>
2023-04-16 13:56:32 -0600

    Delete logOut.php

--- logOut.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    session_start();
-    // remove all session variables
-    session_unset();
-
-    // destroy the session loging the user out
-    session_destroy();
-    header('Location: Login.htm');
-?>
